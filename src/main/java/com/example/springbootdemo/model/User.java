75cde381a6949fcc8394ac3415979414f9e47fca
Dimooon <logu123dima@gmail.com>
2021-12-17 16:35:17 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootdemo/model/User.java
@@ -0,0 +1,24 @@
+package com.example.springbootdemo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+
+
+}
